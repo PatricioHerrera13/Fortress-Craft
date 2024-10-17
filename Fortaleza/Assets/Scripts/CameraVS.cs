c9a6dea0168716e7f741e83064554a087f913a47
Feeergus <jero.sesto@gmail.com>
2024-10-17 11:50:52 -0300

    0.0.32

---
+++ Fortaleza/Assets/Scripts/CameraVS.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraVS : MonoBehaviour
+{
+    public Camera Cam1;
+    public Camera Cam2;
+
+    public float borderSize = 0.02f; // Tamaño del borde (ajustable)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Configuración de pantalla dividida con bordes negros
+        Cam1.rect = new Rect(borderSize, borderSize, 0.5f - borderSize * 2, 1 - borderSize * 2); // Mitad izquierda con borde
+        Cam2.rect = new Rect(0.5f + borderSize, borderSize, 0.5f - borderSize * 2, 1 - borderSize * 2); // Mitad derecha con borde
+
+        // Asignar las posiciones iniciales específicas
+        Cam1.transform.position = new Vector3(0, 15, -20);
+        Cam2.transform.position = new Vector3(-43.5f, 15, -20);
+
+        // Ajustar la rotación inicial de las cámaras (45 grados en el eje X)
+        Cam1.transform.rotation = Quaternion.Euler(45, 0, 0);
+        Cam2.transform.rotation = Quaternion.Euler(45, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Apuntar las cámaras hacia los respectivos jugadores
+        //
+
+        // Mantener la inclinación de 45 grados en el eje X mientras miran a los jugadores
+        Cam1.transform.rotation = Quaternion.Euler(45, Cam1.transform.rotation.eulerAngles.y, 0);
+        Cam2.transform.rotation = Quaternion.Euler(45, Cam2.transform.rotation.eulerAngles.y, 0);
+    }
+}
