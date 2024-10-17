ae38d6993352c7fc8e407eb1725452a97b6b0e96
Feeergus <jero.sesto@gmail.com>
2024-10-17 02:49:02 -0300

    0.0.30

--- Fortaleza/Assets/Scripts/Timer.cs
+++ Fortaleza/Assets/Scripts/Timer.cs
@@ -5,11 +5,14 @@ using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    public float timer = 100;
+    public float timer = 90;
     public Text textoTimer;
     public PortalDeEntregas portal;
     public GameObject PanelFase;
+    public GameObject PanelVic;
     public GameObject PanelDerrota;
+    public float segundos = 2;
+    public float fase1 = 0;
 
     // Update is called once per frame
     void Update()
@@ -20,18 +23,49 @@ public class Timer : MonoBehaviour
             timer -= Time.deltaTime;
 
             textoTimer.text = "00:" + timer.ToString("f0");
+
+            if(portal.cantEntrega >= 2)
+            {
+                //Debug.Log("Calculando...");
+                if(fase1 == 0)
+                {
+                    StartCoroutine(ActivarObjetoPorTiempo());
+                }   
+            }
+
+            if(portal.cantEntrega >= 4)
+            {
+                
+                StartCoroutine(ActivarObjetoPorTiempo());
+            }
         }
         else
         {
             Time.timeScale = 0;
-            if(portal.cantEntrega >= 5)
-            {
-                PanelFase.SetActive(true);
-            }
-            else
+            if(portal.cantEntrega < 5)
             {
                 PanelDerrota.SetActive(true);
             }
         }
+
+    }
+
+    
+    private IEnumerator ActivarObjetoPorTiempo()
+    {
+        if(fase1 == 0)
+        {
+            fase1 = 1;
+            PanelFase.SetActive(true);
+            yield return new WaitForSeconds(segundos); // Espera por el tiempo especificado
+            PanelFase.SetActive(false); // Desactiva el objeto
+        }
+        else
+        {
+            PanelVic.SetActive(true);
+            yield return new WaitForSeconds(segundos); // Espera por el tiempo especificado
+            Time.timeScale = 0;
+        }
+        
     }
 }
