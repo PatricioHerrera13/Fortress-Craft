e82dc3ceffe136e3a0f88a96b829b854da734166
Feeergus <jero.sesto@gmail.com>
2024-10-18 00:09:59 -0300

    0.0.33

--- Fortaleza/Assets/Scripts/DeteccionDisparo.cs
+++ Fortaleza/Assets/Scripts/DeteccionDisparo.cs
@@ -8,25 +8,27 @@ public class DeteccionDisparo : MonoBehaviour
     public GameObject cañon2;  // Cañón izquierdo para player 2
     public Collider p1;
     public Collider p2;
-    public float VelLen = 2f;  // Velocidad lenta (movimiento hacia atrás)
-    public float VelRap = 5f;  // Velocidad rápida (movimiento hacia adelante)
-    public float TiempoCarga = 3f;  // Tiempo de carga antes de disparar
+    public float VelLen = 1f;  // Velocidad lenta (movimiento hacia atrás)
+    public float VelRap = 3f;  // Velocidad rápida (movimiento hacia adelante)
+    public float TiempoCarga = 2f;  // Tiempo de carga antes de disparar
     public bool cambioDir = false;
-    private Vector3 dirDerecha = Vector3.back;  // Movimiento hacia atrás para cañón derecho
-    private Vector3 dirIzquierda = Vector3.back;  // Movimiento hacia atrás para cañón izquierdo
+    private Vector3 dirDerecha = Vector3.left;  // Movimiento hacia atrás para cañón derecho
+    private Vector3 dirIzquierda = Vector3.right;  // Movimiento hacia atrás para cañón izquierdo
     private Vector3 posInicialCañon1;  // Posición inicial del cañón derecho
     private Vector3 posInicialCañon2;  // Posición inicial del cañón izquierdo
+    public GameObject proyectil;
 
     void Start()
     {
         Debug.Log("Pos. Inicial Lista");
         posInicialCañon1 = cañon1.transform.position;
         posInicialCañon2 = cañon2.transform.position;
+        
     }
 
     void OnTriggerStay(Collider other)
     {
-        Debug.Log("Entrando en el trigger con: " + other.tag);
+        //Debug.Log("Entrando en el trigger con: " + other.tag);
 
         if (other.CompareTag("Player"))
         {
@@ -54,7 +56,7 @@ public class DeteccionDisparo : MonoBehaviour
 
         while (tiempo < TiempoCarga)
         {
-            Debug.Log("Retrocediendo " + cañon.name);
+            //Debug.Log("Retrocediendo " + cañon.name);
             cañon.transform.position = Vector3.Lerp(cañon.transform.position, objetivo, tiempo / TiempoCarga);
             tiempo += Time.deltaTime * VelLen;
             yield return null;
@@ -68,10 +70,12 @@ public class DeteccionDisparo : MonoBehaviour
         tiempo = 0f;
         while (tiempo < TiempoCarga)
         {
-            Debug.Log("Disparo de " + cañon.name);
+            //Debug.Log("Disparo de " + cañon.name);
             cañon.transform.position = Vector3.Lerp(cañon.transform.position, posInicial, tiempo / TiempoCarga);
             tiempo += Time.deltaTime * VelRap;
             yield return null;
         }
     }
+
+    
 }
