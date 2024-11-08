389c8276b03faf361001fbd68006d342398d04ba
PatricioHerrera13 <patricioherrera133@gmail.com>
2024-11-08 00:33:56 -0300

    Solucion de errores

--- Fortaleza-ayuda/Assets/Scripts/CRAFTEOS/ItemSOHolder.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-public class ItemSOHolder : MonoBehaviour {
-    public ItemSO itemSO; // Referencia al ScriptableObject ItemSO
-}
--- Fortaleza-ayuda/Assets/Scripts/PrefabSprite.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class PrefabSprite : MonoBehaviour
-{
-    public Sprite sprite;  // Aquí puedes asignar el sprite en el inspector
-}
